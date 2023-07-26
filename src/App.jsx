e4b3af0bd7fe2d0d7daac61ea624f0b8b126cf98
keigo mochiduki <keeee50114@gmail.com>
2023-07-26 20:49:31 +0900

    個数制限機能を追加

--- src/App.jsx
+++ src/App.jsx
@@ -50,7 +50,14 @@ export const App = () => {
         todoText={todoText}
         onChange={onChangeTodoText}
         onClick={onClickAdd}
+        disabled={incompleteTodos.length >= 5}
       />
+      {incompleteTodos.length >= 5 && (
+        <p style={{ color: "red" }}>
+          追加できるTODOは5つまでです。TODOを消化してください。
+        </p>
+      )}
+
       <IncompleteTodos
         todos={incompleteTodos}
         onClickComplete={onClickComplete}
